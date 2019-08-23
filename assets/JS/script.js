e6f6716ceaa926b04be80ef7240b3775f99b58a2
JHaarla <haarladesign@gmail.com>
2019-08-23 01:02:14 -0400

    added show more gifs button - shows 10 more gifs on each click

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -2,16 +2,21 @@ $(document).ready(function(){
 
 // SETUP VARIABLES
 //==========================================
+var offset = 0;
 var authKey = "N28xiqV40WuGfKE0cSITDuibMbKogzMo";
-var apiURLBase = "https://api.giphy.com/v1/gifs/search?api_key=" + authKey + "&limit=10&offset=0&rating=PG-13&q=";
+var apiURLBase = "https://api.giphy.com/v1/gifs/search?api_key=" + authKey + "&limit=10&offset=" + offset + "&rating=PG-13&q=";
 
 var stillURL = "";
 var animatedURL = "";
 
+var queryTerm = "";
+var queryURL = "";
+
+
 // var gifState = "still";
 
 // array of initial values/emotions to be displayed
-var emotions = ["tired", "confused", "mind blown", "hungry", "hangry", "frustrated", "relaxed", "sassy", "suspicious", "happy"];
+var emotions = ["tired", "confused", "mind blown", "hungry", "hangry", "frustrated", "relaxed", "sassy", "suspicious", "happy", "over it"];
 var addedEmotions = [];
 
 // user input to add to buttons/array
@@ -23,145 +28,187 @@ var userAddTerm = "";
 // FUNCTIONS
 //==========================================
 
-//display buttons
-function displayButtons() {
+    //display buttons
+    function displayButtons() {
+        clear();
+        for (var j = 0; j < addedEmotions.length; j++) {
+            //create button for each array item
+            var btnVal = addedEmotions[j];
+            // remove any spaces (not leading or trailing - those were already trimmed) from the user input. If this is not done, the API only returns values for the first term before the space
+            var queryVal = btnVal.replace(/\s+/g, "");
+
+            // var button = '<button class="btn btn-info btn-sm my-btn-margin">' + btnVal + '</button>';
+            var newBtn = $("<button>").text(btnVal).attr("class", "btn btn-info btn-sm my-btn-margin my-emotion").attr("value", queryVal);
+
+            $("#buttonsDiv").append(newBtn);
+            // console.log(btnVal);
+        }
+        }
+
+
+        // AJAX call function & display GIFs
+        // function getGIFs(){
+            // var queryURL = apiURLBase + userAddTerm;
+            // console.log(queryURL);
+            // $.ajax({
+            //     url: queryURL,
+            //     method: "GET"
+            // }).then(function(dataReturn) {
+            //         console.log(dataReturn);
+                    
+            //     });
+
+        // }
+
+        //empty the buttons div and rewite the buttons to the Dom with the user added button
+        function clear() {
+            $("#buttonsDiv").empty();
+            for (var i = 0; i < emotions.length; i++) {
+                //create button for each array item
+                var btnVal = emotions[i];
+                var queryVal = btnVal.replace(/\s+/g, "");
+                // var button = '<button class="btn btn-info btn-sm my-btn-margin">' + btnVal + '</button>';
+                var newBtn = $("<button>").text(btnVal).attr("class", "btn btn-info btn-sm my-btn-margin my-emotion").attr("value", queryVal);
+
+                $("#buttonsDiv").append(newBtn);
+                // console.log(btnVal);
+            }  
+        }
+
+
+    // MAIN PROCESS
+    //==========================================
+
+    // show the initial buttons
     clear();
-    for (var j = 0; j < addedEmotions.length; j++) {
-        //create button for each array item
-        var btnVal = addedEmotions[j];
-        // remove any spaces (not leading or trailing - those were already trimmed) from the user input. If this is not done, the API only returns values for the first term before the space
-        var queryVal = btnVal.replace(/\s+/g, "");
 
-        // var button = '<button class="btn btn-info btn-sm my-btn-margin">' + btnVal + '</button>';
-        var newBtn = $("<button>").text(btnVal).attr("class", "btn btn-info btn-sm my-btn-margin my-emotion").attr("value", queryVal);
+    // hide gif-container until something is clicked
+    $(".gif-container").hide();
 
-        $("#buttonsDiv").append(newBtn);
-        // console.log(btnVal);
-    }
+    // on.click event for the buttons up top - call the Giphy API and return 10 GIFs to the DOM
 
-}
+    $("#buttonsDiv").on("click", ".my-emotion", function(){ //event delegation syntax for the on.click event to read dynamically added buttons
+    console.log($(this).val());
+    // getGIFs();
+    offset = 0;
+    queryTerm = $(this).val();
+    queryURL = apiURLBase + queryTerm;
+
+    console.log(queryURL); 
+    // the ajax call and promise
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(dataReturn) {
+        $(".gif-container").show();
+
+        $(".gif-div").remove();
+        // console.log(dataReturn);
+        //this stores the data (the bit we're interested in) in a variable
+        var results = dataReturn.data;
+        //     
+        // console.log(results);
+        for (var k = 0; k < results.length; k++) {
+            stillURL = results[k].images.fixed_height_still.url;
+            animatedURL = results[k].images.fixed_height.url;
+            var rating = results[k].rating;
+        console.log(stillURL);
+        console.log(animatedURL);
+
+            var GIFdiv = $("<div>").addClass("gif-div");
+            var paraEl = $("<p>").text("Rating: " + rating).addClass("rating");
+            var emotionGIF = $("<img>");
+            emotionGIF.attr("src", stillURL).attr("gif-still", stillURL).attr("gif-animated", animatedURL).attr("gif-state", "still").addClass("gif");
+
+            GIFdiv.prepend(paraEl);
+            GIFdiv.append(emotionGIF);
+
+            $("#gifdiv").append(GIFdiv); 
+        }
+        });
+    })
 
 
-// AJAX call function & display GIFs
-// function getGIFs(){
-    // var queryURL = apiURLBase + userAddTerm;
-    // console.log(queryURL);
-    // $.ajax({
-    //     url: queryURL,
-    //     method: "GET"
-    // }).then(function(dataReturn) {
-    //         console.log(dataReturn);
-            
-    //     });
+    // on.click event that adds another button
+    $("#userInputBtn").on("click", function(event) {
+    // $(document).on("click", "#userInputBtn", function(event) {
 
-// }
+        // Blocks an empty button to be displayed if the user clicks it without adding any text
+            //Doesn't work for some reason...boooo
+        event.preventDefault();
+        //Grab user input & trim it
+        userAddTerm = $("#addInput").val().trim();
+        // push the user added input to addedEmotions array
+            // if conditional is to prevent and empty button since event.preventDefault(); didn't work...
+        if (userAddTerm !== "") {
+        addedEmotions.push(userAddTerm);
+        console.log(userAddTerm);
+        // reset input field to nothing so it can't be clicked repeadetly adding the last item over and over
+        $("#addInput").val("");
+        // trigger the displayButtons functions to write all buttons to DOM
+        displayButtons();
+        }
+    })
 
-//empty the buttons div and rewite the buttons to the Dom with the user added button
-function clear() {
-    $("#buttonsDiv").empty();
-    for (var i = 0; i < emotions.length; i++) {
-        //create button for each array item
-        var btnVal = emotions[i];
-        var queryVal = btnVal.replace(/\s+/g, "");
-        // var button = '<button class="btn btn-info btn-sm my-btn-margin">' + btnVal + '</button>';
-        var newBtn = $("<button>").text(btnVal).attr("class", "btn btn-info btn-sm my-btn-margin my-emotion").attr("value", queryVal);
+    // click event to listen for click on the still image so we can toggle animate/still of the gif
+    $("#gifdiv").on("click", ".gif", function(){
+    state = $(this).attr("gif-state");
 
-        $("#buttonsDiv").append(newBtn);
-        // console.log(btnVal);
-    }  
-}
+    if (state === "still") {
+        $(this).attr("gif-state", "animated");
+        $(this).attr("src", $(this).attr("gif-animated"));
+    } else if (state === "animated") {
+        $(this).attr("gif-state", "still");
+        $(this).attr("src", $(this).attr("gif-still"));
 
-
-// MAIN PROCESS
-//==========================================
-
-// show the initial buttons
-clear();
-
-// hide gif-container until something is clicked
-$(".gif-container").hide();
-
-// on.click event for the buttons up top - call the Giphy API and return 10 GIFs to the DOM
-
-$("#buttonsDiv").on("click", ".my-emotion", function(){ //event delegation syntax for the on.click event to read dynamically added buttons
-console.log($(this).val());
-// getGIFs();
-
-var queryTerm = $(this).val();
-var queryURL = apiURLBase + queryTerm;
-// console.log(queryURL); 
-// the ajax call and promise
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function(dataReturn) {
-    $(".gif-container").show();
-
-    $(".gif-div").remove();
-    // console.log(dataReturn);
-    //this stores the data (the bit we're interested in) in a variable
-    var results = dataReturn.data;
-    //     
-    // console.log(results);
-    for (var k = 0; k < results.length; k++) {
-        stillURL = results[k].images.fixed_height_still.url;
-        animatedURL = results[k].images.fixed_height.url;
-        var rating = results[k].rating;
-    console.log(stillURL);
-    console.log(animatedURL);
-
-        var GIFdiv = $("<div>").addClass("gif-div");
-        var paraEl = $("<p>").text("Rating: " + rating).addClass("rating");
-        var emotionGIF = $("<img>");
-        emotionGIF.attr("src", stillURL).attr("gif-still", stillURL).attr("gif-animated", animatedURL).attr("gif-state", "still").addClass("gif");
-
-        GIFdiv.prepend(paraEl);
-        GIFdiv.append(emotionGIF);
-
-        $("#gifdiv").append(GIFdiv); 
     }
     });
-})
 
 
-// on.click event that adds another button
-$("#userInputBtn").on("click", function(event) {
-// $(document).on("click", "#userInputBtn", function(event) {
+    // show more GIFs button functionality added here - adds 10 more gifs of the already chosen GIF
 
-    // Blocks an empty button to be displayed if the user clicks it without adding any text
-        //Doesn't work for some reason...boooo
-    event.preventDefault();
-    //Grab user input & trim it
-    userAddTerm = $("#addInput").val().trim();
-    // push the user added input to addedEmotions array
-        // if conditional is to prevent and empty button since event.preventDefault(); didn't work...
-    if (userAddTerm !== "") {
-    addedEmotions.push(userAddTerm);
-    console.log(userAddTerm);
-    // reset input field to nothing so it can't be clicked repeadetly adding the last item over and over
-    $("#addInput").val("");
-    // trigger the displayButtons functions to write all buttons to DOM
-    displayButtons();
-    }
-})
+    $(".beforeGIF").on("click", "#moreGIF", function(){ //event delegation syntax for the on.click event to read dynamically added buttons
+        console.log(queryTerm);
 
-// click event to listen for click on the still image so we can toggle animate/still of the gif
-$("#gifdiv").on("click", ".gif", function(){
-state = $(this).attr("gif-state");
+        offset += 10;
+        var moreGifsURL = "https://api.giphy.com/v1/gifs/search?api_key=" + authKey + "&limit=10&offset=" + offset + "&rating=PG-13&q=";
+        // queryTerm = $(this).val();
+        
+        queryURL = moreGifsURL + queryTerm;
+        console.log(queryURL);
 
-if (state === "still") {
-    $(this).attr("gif-state", "animated");
-    $(this).attr("src", $(this).attr("gif-animated"));
-} else if (state === "animated") {
-    $(this).attr("gif-state", "still");
-    $(this).attr("src", $(this).attr("gif-still"));
+        
+        // the ajax call and promise
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(dataReturn) {
+            // $(".gif-container").show();
+        //     $(".gif-div").remove();
 
-}
-});
-
-
-
-
+        //     // console.log(dataReturn);
+        //     //this stores the data (the bit we're interested in) in a variable
+            var results = dataReturn.data;
+        //     // console.log(results);
+            for (var k = 0; k < results.length; k++) {
+                stillURL = results[k].images.fixed_height_small_still.url;
+                animatedURL = results[k].images.fixed_height_small.url;
+                var rating = results[k].rating;
+            console.log(stillURL);
+            console.log(animatedURL);
+        
+                var GIFdiv = $("<div>").addClass("gif-div");
+                var paraEl = $("<p>").text("Rating: " + rating).addClass("rating");
+                var emotionGIF = $("<img>");
+                emotionGIF.attr("src", stillURL).attr("gif-still", stillURL).attr("gif-animated", animatedURL).attr("gif-state", "still").addClass("gif");
+        
+                GIFdiv.prepend(paraEl);
+                GIFdiv.append(emotionGIF);
+        
+                $("#gifdiv").prepend(GIFdiv); 
+            }
+            });
+        })
 
 
 
